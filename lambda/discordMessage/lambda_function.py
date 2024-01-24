2783a7822e143f7d4f4043f8f8744c13ab0de7dc
f1nn1711 <49924040+f1nn1711@users.noreply.github.com>
2024-01-24 18:27:51 +0000

    Update lambda_function.py

--- lambda/discordMessage/lambda_function.py
+++ lambda/discordMessage/lambda_function.py
@@ -3,6 +3,43 @@ import os
 import json
 
 
+def getMessageContent(tradeType, messageData):
+    signal, ticker = messageData.split('/')
+    url = f'https://{tradeType}.trading212.com/?ticker={ticker}'
+    colour = 65340
+    if signal == 'SELL':
+        colour = 16711680
+
+    discordMessage = {
+        "embeds": [
+            {
+                "type": "rich",
+                "description": "Entry point identified",
+                "color": colour,
+                "url": url,
+                "fields": [
+                    {
+                        "name": "Signal",
+                        "value": signal,
+                        "inline": False
+                    },
+                    {
+                        "name": "Ticker",
+                        "value": ticker,
+                        "inline": False
+                    },
+                    {
+                        "name": "Link",
+                        "value": url,
+                        "inline": False
+                    }
+                ]
+            }
+        ]
+    }
+
+    return discordMessage
+
 def lambda_handler(event, context):
     discordUrl = 'https://discord.com/api/webhooks/1199342199062659172/dzQzhZ7hpQYmZaK-p0kq2uSqzMrc2IlwdFkclsR5C77RkWzaPVRm_dnOsNywougLRiKF'
     if os.environ.get('DISCORD_URL') is not None:
@@ -15,39 +52,12 @@ def lambda_handler(event, context):
     for record in event['Records']:
         payload = record["body"]
 
-        signal, ticker = payload.split('/')
-        url = f'https://{tradeType}.trading212.com/?ticker={ticker}'
-        colour = 65340
-        if signal == 'SELL':
-            colour = 16711680
-
-        discordMessage = {
-            "embeds": [
-                {
-                    "type": "rich",
-                    "description": "Entry point identified",
-                    "color": colour,
-                    "url": url,
-                    "fields": [
-                        {
-                            "name": "Signal",
-                            "value": signal,
-                            "inline": False
-                        },
-                        {
-                            "name": "Ticker",
-                            "value": ticker,
-                            "inline": False
-                        },
-                        {
-                            "name": "Link",
-                            "value": url,
-                            "inline": False
-                        }
-                    ]
-                }
-            ]
-        }
+        try:
+            discordMessage = getMessageContent(tradeType, payload)
+        except:
+            discordMessage = {
+                'content': f'Failed processing payload: {json.dumps(payload)}'
+            }
 
         response = requests.post(discordUrl, json=discordMessage)
 
