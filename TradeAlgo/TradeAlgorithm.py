cc43f3c30cf68b53f4dd00e44f52e8d9f397b8fe
f1nn1711 <49924040+f1nn1711@users.noreply.github.com>
2024-01-26 09:01:20 +0000

    Updating to implement correct algo

--- TradeAlgo/TradeAlgorithm.py
+++ TradeAlgo/TradeAlgorithm.py
@@ -3,6 +3,43 @@ from DataProcessor import Processor
 from helpers import isSet
 import pandas as pd
 import pandas_ta as pdTa
+import MessageHandler
+import json
+
+
+
+def checkIfCallExists(callCode: str) -> bool:
+    with open('./TradeAlgo/data/currentCalls.json', 'r') as f:
+        currentCalls = json.loads(f.read())
+        f.close()
+
+    if callCode in currentCalls['calls']:
+        return True
+
+    return False
+
+
+def updateCurrentCalls(callCode: str) -> None:
+    with open('./TradeAlgo/data/currentCalls.json', 'r') as f:
+        currentCalls = json.loads(f.read())
+        f.close()
+
+    with open('./TradeAlgo/data/currentCalls.json', 'w') as f:
+        currentCalls['calls'].append(callCode)
+
+        f.write(json.dumps(currentCalls))
+        f.close()
+
+def removeCurrentCall(callCode: str) -> None:
+    with open('./TradeAlgo/data/currentCalls.json', 'r') as f:
+        currentCalls = json.loads(f.read())
+        f.close()
+
+    with open('./TradeAlgo/data/currentCalls.json', 'w') as f:
+        currentCalls['calls'].remove(callCode)
+
+        f.write(json.dumps(currentCalls))
+        f.close()
 
 
 class TradeAlgorithm:
@@ -14,6 +51,9 @@ class TradeAlgorithm:
         return self.tickers
 
     def getResult(self):
+        # help(pdTa.adx)
+        # help(pdTa.bbands)
+        # help(pdTa.rsi)
         results = []
 
         tickerData = []
@@ -28,6 +68,60 @@ class TradeAlgorithm:
 
         labeledData = [[self.processor.applyDataSchema(data) for data in ticker] for ticker in tickerData]
 
+        handler = MessageHandler.Messager()
+        callsMade = []
+        for ticker, data in zip(self.getTickers(), labeledData):
+            result = self.getTickerResult(ticker, data)
+
+            if result == -1 and not checkIfCallExists(f'SELL/{ticker}'):
+                handler.enqueue(f'SELL/{ticker}', 'MessageQueue-dev')
+                updateCurrentCalls(f'SELL/{ticker}')
+                continue
+
+            if result == 1 and not checkIfCallExists(f'BUY/{ticker}'):
+                handler.enqueue(f'BUY/{ticker}', 'MessageQueue-dev')
+                updateCurrentCalls(f'BUY/{ticker}')
+                continue
+
+            if result == 0:
+                if checkIfCallExists(f'BUY/{ticker}'):
+                    removeCurrentCall(f'BUY/{ticker}')
+                    continue
+
+                if checkIfCallExists(f'SELL/{ticker}'):
+                    removeCurrentCall(f'SELL/{ticker}')
+
         return results
+
+    def getTickerResult(self, ticker: str, data: list) -> int:
+        if ticker == 'NZDUSD':
+            return 0
+
+        df = pd.DataFrame.from_dict(data)
+
+        strategy = pdTa.Strategy('Strategy v1', ta=[
+            {"kind": "adx", "length": 14},
+            {"kind": "rsi", "length": 14},
+            {"kind": "bbands", "length": 20, "std": 2}
+        ])
+
+        df.ta.strategy(strategy)
+
+        adxValue = float(df.tail(1)['ADX_14'].iloc[0])
+        if adxValue < 25:
+            return 0
+
+        currentPrice = float(df.tail(1)['close'].iloc[0])
+        bbUpper = float(df.tail(1)['BBU_20_2.0'].iloc[0])
+        bbLower = float(df.tail(1)['BBL_20_2.0'].iloc[0])
+        rsiValue = float(df.tail(1)['RSI_14'].iloc[0])
+
+        if rsiValue <= 30 and currentPrice < bbLower:
+            return 1
+
+        if rsiValue >= 70 and currentPrice > bbUpper:
+            return -1
+
+        return 0
 
 
